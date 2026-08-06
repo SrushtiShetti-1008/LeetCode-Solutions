442391e5932039b0d26dfd7fdb2937fa1c32d39c
SrushtiShetti-1008 <srushtishetti1008@gmail.com>
2026-08-06 23:16:05 +0530

    Added Leetcode Solution

---
+++ LeetCode-75/reverse_vowels.py
@@ -0,0 +1,15 @@
+class Solution:
+    def reverseVowels(self, s: str) -> str:
+        l=list(s)
+        i,j=0,len(l)-1
+        while i<j:
+            if l[i] not in 'aeiouAEIOU':
+                i+=1
+            if l[j] not in 'aeiouAEIOU':
+                j-=1
+            if l[i] in 'aeiouAEIOU' and l[j] in 'aeiouAEIOU':
+                l[i],l[j] = l[j],l[i]
+                i+=1
+                j-=1
+        return ''.join(l)
+       c
